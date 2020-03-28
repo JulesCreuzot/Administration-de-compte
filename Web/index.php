303cffd1887be505f4dc938646ce3b1d238782a9
JulesCreuzot <julescreuzotbobo@gmail.com>
2020-03-28 16:00:55 +0100

    Ajout du site

---
+++ Web/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Votre Commentaire Journalier | Accueil</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="icon" type="image/png" href="#">
+
+    <link rel="stylesheet" href="content/css/bootstrap.min.css">
+    <link rel="stylesheet" href="content/css/navbar.css">
+
+    <script src="content/js/bootstrap.min.js"></script>
+    <script src="content/js/jquery.min.js"></script>
+    <script src="content/js/navbar.js"></script>
+
+</head>
+<body>
+<div class="navmenu navmenu-default navmenu-fixed-left offcanvas lien">
+    <div style="padding: 15px"></div>
+    <div class="navmenu-brand">Votre Commentaire Journalier</div>
+    <div style="padding: 20px"></div>
+    <ul class="nav navmenu-nav">
+        <li><a href="#">Comming Soon...</a></li>
+    </ul>
+    <div style=" margin-top: 10%"></div>
+    <ul class="nav navmenu-nav">
+        <li><a href="#"> <p>Mentions légales</p></a></li>
+    </ul>
+</div>
+
+<div class="navbar navbar-default navbar-fixed-top">
+    <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".navmenu" data-canvas="body">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+</div>
+
+<div class="container">
+    <div class="page-header">
+        <h1>Projet SI6 - Gestion d'un compte</h1>
+    </div>
+    <p class="lead">C'est une page d'exemple</p>
+    <p>Seulement le style général du site à été réaliser; Des modification aurons lieu prochainement</p>
+</div>
+
+<footer class="container-fluid text-center">
+
+    <div class="col-md-4 text-left" style="font-size: 15px">
+        <p style="padding-top: 35px; padding-left: 2%">© 2020, VCM | Par les BTS SIO - Tout droit réservés </p>
+    </div>
+    <div class="col-md-4 text-center">
+        <div style="padding: 5px"></div>
+        <img src="content/img/logovaj.png" class="" alt="Logo VAJ">
+        <div style="padding: 5px"></div>
+    </div>
+    <div class="col-md-4 text-right">
+    </div>
+</footer>
+
+</body>
+</html>
