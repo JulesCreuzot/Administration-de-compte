0b73e97f7b4bc7f93e7cc880edc76ecfe31d0b4a
JulesCreuzot <julescreuzotbobo@gmail.com>
2020-03-28 16:25:47 +0100

    Optimisation du code
    - Modification du nom de fichier NavBar.css en Nav.css -- Il contient tout le CSS nécessaire à la navigation
    - Ajout du fichier  NavBar.php, il contient  la barre de navigation à droite
    - Ajout du fichier PiedPage.php, il contient le pied de page
    - Modification du fichier index :+1:  , à savoir :
          - Ajout du code permettant d'aller chercher la page navbar et de la page piedpage
          - Suppression des ligne de code contenant le pied de page et la bare de navigation
    
    Le CSS ainsie que le JS nécessaire au chargement du contenue de "navigation" seront maintenant chargé dans le fichier navbar.php

---
+++ Web/content/Nav/navbar.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="content/css/nav.css">
+
+<div class="navmenu navmenu-default navmenu-fixed-left offcanvas lien">
+    <div style="padding: 15px"></div>
+    <div class="navmenu-brand">Votre Commentaire Journalier</div>
+    <div style="padding: 20px"></div>
+    <ul class="nav navmenu-nav">
+        <li><a href="#">Comming Soon...</a></li>
+    </ul>
+    <div style=" margin-top: 10%"></div>
+    <ul class="nav navmenu-nav">
+        <li><a href="#"> <p>Mentions légales</p></a></li>
+    </ul>
+</div>
+
+<div class="navbar navbar-default navbar-fixed-top">
+    <button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".navmenu" data-canvas="body">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+</div>
---
+++ Web/content/Nav/piedpage.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid text-center">
+
+    <div class="col-md-4 text-left" style="font-size: 15px">
+        <p style="padding-top: 35px; padding-left: 2%">© 2020, VCM | Par les BTS SIO - Tout droit réservés </p>
+    </div>
+    <div class="col-md-4 text-center">
+        <div style="padding: 5px"></div>
+        <img src="content/img/logovaj.png" class="" alt="Logo VAJ">
+        <div style="padding: 5px"></div>
+    </div>
+    <div class="col-md-4 text-right">
+    </div>
+</footer>
